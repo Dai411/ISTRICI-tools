dff3aec0764791562b46a580fc62b2998783ceaa
Rock411 <yangln@126.com>
2025-07-08 14:49:23 +0200

    Update CIG_extract.sh

--- VelocityAnalysis/CIG_extract.sh
+++ VelocityAnalysis/CIG_extract.sh
@@ -2,6 +2,10 @@
 
 # ==============================================================================
 # Script Name: CIG_extract.sh
+# Version: 1.1
+# Author: Lining YANG, CNR-ISMAR Bologna
+# Date: 2025-07-08 20:00
+
 # Description: Extracts CDP traces from seismic data files and creates
 #              output files with simplified names. Supports progress animation.
 #
